b11d976b355fc68e0a03b8ad87b571fc401d1cc1
seaweed2 <winseawoo@naver.com>
2023-06-01 23:36:30 +0900

    gps_loading

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,140 @@
+package com.example.myapplication;
+
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private TextView txtLatitude;
+    private TextView txtLongitude;
+    private LocationManager locationManager;
+    private Geocoder geocoder;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtLatitude = findViewById(R.id.txtLatitude);
+        txtLongitude = findViewById(R.id.txtLongitude);
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        geocoder = new Geocoder(this, Locale.getDefault());
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request location permissions if not granted
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION, android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        } else {
+            // Permission is already granted, proceed with obtaining location
+            obtainLocation();
+
+            // Request location updates
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, this);
+        }
+    }
+
+    private void obtainLocation() {
+        Location location;
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        // Perform necessary tasks using the location information
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            String latitudeText = getString(R.string.latitude_text, latitude);
+            String longitudeText = getString(R.string.longitude_text, longitude);
+
+            txtLatitude.setText(latitudeText);
+            txtLongitude.setText(longitudeText);
+
+            try {
+                List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 10);
+                if (addresses != null && addresses.size() > 0) {
+                    String addressLine = addresses.get(0).getAddressLine(0);
+                    Log.e("위치", addresses.get(0).toString());
+                    Log.e("주소", addressLine);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Location permissions granted, proceed with obtaining location
+                obtainLocation();
+            } else {
+                Toast.makeText(this, "Location permissions denied", Toast.LENGTH_SHORT).show();
+                // Location permissions denied, handle accordingly
+            }
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        String latitudeText = getString(R.string.latitude_text, latitude);
+        String longitudeText = getString(R.string.longitude_text, longitude);
+
+        txtLatitude.setText(latitudeText);
+        txtLongitude.setText(longitudeText);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 10);
+            if (addresses != null && addresses.size() > 0) {
+                String addressLine = addresses.get(0).getAddressLine(0);
+                Log.e("위치", addresses.get(0).toString());
+                Log.e("주소", addressLine);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {}
+
+    @Override
+    public void onProviderEnabled(String provider) {}
+
+    @Override
+    public void onProviderDisabled(String provider) {}
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MainActivity", "onDestroy() called");
+    }
+}
