f3291616b363513f5f31dc6d1135855942cc006e
seaweed2 <winseawoo@naver.com>
2023-06-08 10:30:50 +0900

    gps button 삭제 // 위도,경도 갱신 확인완료 timeActivity와 MainActivity 병합. time, main xml 병합. 현재 위도, 경도, 현재시간 출력 함수 작성되어 있습니다.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
@@ -13,22 +16,29 @@ import androidx.core.app.ActivityCompat;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
-
+public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
     private TextView txtLatitude;
     private TextView txtLongitude;
     private LocationManager locationManager;
     private Geocoder geocoder;
+    long mNow;
+    Date mDate;
+    SimpleDateFormat mFormat;
+    private TextView txtime;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //생성자
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -37,6 +47,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         geocoder = new Geocoder(this, Locale.getDefault());
+
+        //bind view --- time
+        txtime = findViewById(R.id.txtime);
+        Button timebtn = findViewById(R.id.timebtn);
+        //bind listener
+        timebtn.setOnClickListener(this);
+
+        mFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault());
+        // --- time
 
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -50,32 +69,39 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 1, this);
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000, 1, this);
         }
+
     }
 
     private void obtainLocation() {
         Location location;
 
+        // 위치 권한이 허용되지 않았을 경우 메서드 종료
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             return;
         }
 
+        // GPS 공급자를 사용하여 마지막으로 알려진 위치를 가져옴
         location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        // Perform necessary tasks using the location information
+        // 위치 정보를 사용하여 필요한 작업 수행
         if (location != null) {
             double latitude = location.getLatitude();
             double longitude = location.getLongitude();
 
+            // 위도와 경도를 포맷팅된 문자열로 변환
             String latitudeText = getString(R.string.latitude_text, latitude);
             String longitudeText = getString(R.string.longitude_text, longitude);
 
+            // 텍스트 뷰에 위도와 경도를 설정하여 화면에 표시
             txtLatitude.setText(latitudeText);
             txtLongitude.setText(longitudeText);
 
             try {
+                // 위도와 경도에 해당하는 주소 목록을 최대 10개까지 가져옴
                 List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 10);
                 if (addresses != null && addresses.size() > 0) {
+                    // 첫 번째 주소의 주소 문자열과 전체 주소 정보를 로그로 출력
                     String addressLine = addresses.get(0).getAddressLine(0);
                     Log.e("위치", addresses.get(0).toString());
                     Log.e("주소", addressLine);
@@ -84,18 +110,72 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
                 e.printStackTrace();
             }
         }
+
+        /*if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            String currentTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault()).format(new Date());
+
+            // 데이터베이스에 위치 정보 삽입
+            gps_db dbHelper = new gps_db(MainActivity.this);
+            dbHelper.insertLocation(latitude, longitude, currentTime);
+
+            // 현재 날짜 가져오기
+            String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+            // 데이터베이스 초기화
+            if (!dbHelper.checkDateExists(currentDate)) {
+                dbHelper.resetDatabase();
+                dbHelper.insertDate(currentDate);
+            }
+        }*/
     }
+
+    //---time
+    private String getTime() {
+        mNow = System.currentTimeMillis();
+        mDate = new Date(mNow);
+        return mFormat.format(mDate);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.timebtn) {
+            txtime.setText(getTime());
+        } else {
+            txtime.setText("문자열 변환");
+        }
+        /*if (v.getId() == R.id.timebtn) {
+            String currentTime = getTime();
+            String currentDate = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault()).format(new Date());
+
+            // 데이터베이스에 현재 위치 정보 삽입
+            gps_db dbHelper = new gps_db(time.this);
+            dbHelper.insertLocation(latitude, longitude, currentTime);
+
+            // 데이터베이스 초기화
+            if (!dbHelper.checkDateExists(currentDate)) {
+                dbHelper.resetDatabase();
+                dbHelper.insertDate(currentDate);
+            }
+        }*/
+    }
+
+    //---time
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == 1) {
+            // 위치 권한 요청의 결과를 처리
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Location permissions granted, proceed with obtaining location
+                // 위치 권한이 허용된 경우, 위치 정보를 가져오는 작업 계속 진행
                 obtainLocation();
             } else {
+                // 위치 권한이 거부된 경우, 해당 처리를 수행
                 Toast.makeText(this, "Location permissions denied", Toast.LENGTH_SHORT).show();
-                // Location permissions denied, handle accordingly
+                // 위치 권한 거부에 대한 처리를 추가적으로 구현할 수 있음
             }
         }
     }
@@ -104,12 +184,15 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     public void onLocationChanged(Location location) {
         double latitude = location.getLatitude();
         double longitude = location.getLongitude();
+        //새로운 위치의 위도와 경도를 가져옴.
 
         String latitudeText = getString(R.string.latitude_text, latitude);
         String longitudeText = getString(R.string.longitude_text, longitude);
+        //형식화된 문자열을 가져와서 위도와 경도 값을 대체
 
         txtLatitude.setText(latitudeText);
         txtLongitude.setText(longitudeText);
+        //화면에 표시
 
         try {
             List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 10);
@@ -124,13 +207,16 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {}
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+    }
 
     @Override
-    public void onProviderEnabled(String provider) {}
+    public void onProviderEnabled(String provider) {
+    }
 
     @Override
-    public void onProviderDisabled(String provider) {}
+    public void onProviderDisabled(String provider) {
+    }
 
     @Override
     protected void onDestroy() {
